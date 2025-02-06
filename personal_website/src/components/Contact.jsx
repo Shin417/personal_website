0724821d0f1c71d435feace6ef1cfb4c69a58d3b
Shin417 <shintaroasoemail@gmail.com>
2025-02-06 03:25:02 -0500

    deleted email js and switched to web3form.

--- personal_website/src/components/Contact.jsx
+++ personal_website/src/components/Contact.jsx
@@ -1,32 +1,82 @@
-import React from 'react'
-import emailjs from '@emailjs/browser'
+import React from "react";
+import Swal from "sweetalert2";
 
-export default function Contact(){
-    const sendEmail = (e) => {
-        e.preventDefault();
-        emailjs.sendForm('service_53fit8u', 'template_qh3cwby', e.target, 'AIF0v5GbQdtZt8Xq9');
+export default function Contact() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "9ffc0df9-044f-4349-8a6a-a38060591e47");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+        Swal.fire({
+            title: "Message sent successfully!",
+            icon: "success",
+            draggable: false
+          });
+    } else {
+        Swal.fire({
+            title: "Error! Something is going wrong!",
+            icon: "error",
+            draggable: false,
+          });
     }
+  };
 
-    return(
-        <div className='contact-section' id='contact'>
-            <h1>Contact</h1>
-            <div className='contact-content'>
-                <form onSubmit={sendEmail}>
-                    <div className='input-box'>
-                        <label htmlFor='name'>Full Name</label>
-                        <input type="text" name='name' className='input-field' placeholder='Your name' required/>
-                    </div>
-                    <div className='input-box'>
-                        <label htmlFor='email'>Email Address</label>
-                        <input type="email" name='email' className='input-field' placeholder='Email address' required/>
-                    </div>
-                    <div className='input-box'>
-                        <label htmlFor='message'>Message</label>
-                        <textarea name="message" id="message" className='message-field' cols={30} rows={5} placeholder='Message...' required></textarea>
-                    </div>
-                    <button type='submit' className='contact-submit-btn'>Submit</button>
-                </form>
-            </div>
-        </div>
-    );
+  
+
+  return (
+    <div className="contact-section" id="contact">
+      <h1>Contact</h1>
+      <div className="contact-content">
+        <form onSubmit={onSubmit}>
+          <div className="input-box">
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              className="input-field"
+              placeholder="Your name"
+              required
+            />
+          </div>
+          <div className="input-box">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              name="email"
+              className="input-field"
+              placeholder="Email address"
+              required
+            />
+          </div>
+          <div className="input-box">
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              className="message-field"
+              cols={30}
+              rows={5}
+              placeholder="Message..."
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className="contact-submit-btn">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
